3bdb2d6a01dff5d17e37ab965dab64e073db4682
Tessil <tessil@gmx.com>
2016-08-29 11:28:00 +0200

    Set rehash method public

--- src/hopscotch_map.h
+++ src/hopscotch_map.h
@@ -577,6 +577,16 @@ public:
         return (1.0f*m_nb_elements)/m_buckets.size();
     }
     
+    void rehash(size_type bucket_count) {
+        hopscotch_map tmp_map(bucket_count);
+        
+        for(auto && key_value : *this) {
+            tmp_map.insert(std::move(key_value));
+        }
+        
+        std::swap(*this, tmp_map);
+    }
+    
     
     /*
      * Observers
@@ -644,16 +654,6 @@ private:
         }
         
         return iterator(it_next, m_buckets.end(), m_overflow_elements.begin()); 
-    }
-    
-    void rehash(size_type bucket_count) {
-        hopscotch_map tmp_map(bucket_count);
-        
-        for(auto && key_value : *this) {
-            tmp_map.insert(std::move(key_value));
-        }
-        
-        std::swap(*this, tmp_map);
     }
     
     
