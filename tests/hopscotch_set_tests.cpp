c7e01257a0d19729e1d50aebe6ca71d8267d05ba
Tessil <tessil@gmx.com>
2018-05-27 20:59:37 +0200

    Add tests.

--- tests/hopscotch_set_tests.cpp
+++ tests/hopscotch_set_tests.cpp
@@ -35,12 +35,14 @@
 
 BOOST_AUTO_TEST_SUITE(test_hopscotch_set)
  
-using test_types = boost::mpl::list<tsl::hopscotch_set<int64_t, mod_hash<9>>,
+using test_types = boost::mpl::list<tsl::hopscotch_set<std::int64_t, mod_hash<9>>,
                                     tsl::hopscotch_set<self_reference_member_test, mod_hash<9>>,
                                     tsl::hopscotch_set<move_only_test, mod_hash<9>>,
-                                    tsl::bhopscotch_set<int64_t, mod_hash<9>>,
+                                    tsl::hopscotch_pg_set<move_only_test, mod_hash<9>>,
+                                    tsl::bhopscotch_set<std::int64_t, mod_hash<9>>,
                                     tsl::bhopscotch_set<self_reference_member_test, mod_hash<9>>,
-                                    tsl::bhopscotch_set<move_only_test, mod_hash<9>>>;
+                                    tsl::bhopscotch_set<move_only_test, mod_hash<9>>,
+                                    tsl::bhopscotch_pg_set<move_only_test, mod_hash<9>>>;
                                     
                               
                                     
@@ -50,12 +52,12 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(test_insert, HSet, test_types) {
     // insert x values, insert them again, check values
     using key_t = typename HSet::key_type;
     
-    const size_t nb_values = 1000;
+    const std::size_t nb_values = 1000;
     HSet set;
     typename HSet::iterator it;
     bool inserted;
     
-    for(size_t i = 0; i < nb_values; i++) {
+    for(std::size_t i = 0; i < nb_values; i++) {
         std::tie(it, inserted) = set.insert(utils::get_key<key_t>(i));
         
         BOOST_CHECK_EQUAL(*it, utils::get_key<key_t>(i));
@@ -63,18 +65,37 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(test_insert, HSet, test_types) {
     }
     BOOST_CHECK_EQUAL(set.size(), nb_values);
     
-    for(size_t i = 0; i < nb_values; i++) {
+    for(std::size_t i = 0; i < nb_values; i++) {
         std::tie(it, inserted) = set.insert(utils::get_key<key_t>(i));
         
         BOOST_CHECK_EQUAL(*it, utils::get_key<key_t>(i));
         BOOST_CHECK(!inserted);
     }
     
-    for(size_t i = 0; i < nb_values; i++) {
+    for(std::size_t i = 0; i < nb_values; i++) {
         it = set.find(utils::get_key<key_t>(i));
         
         BOOST_CHECK_EQUAL(*it, utils::get_key<key_t>(i));
     }
+}
+
+BOOST_AUTO_TEST_CASE(test_compare) {
+    const tsl::hopscotch_set<std::string> set1_1 = {"a", "e", "d", "c", "b"};
+    const tsl::hopscotch_set<std::string> set1_2 = {"e", "c", "b", "a", "d"};
+    const tsl::hopscotch_set<std::string> set2_1 = {"e", "c", "b", "a", "d", "f"};
+    const tsl::hopscotch_set<std::string> set3_1 = {"e", "c", "b", "a"};
+    
+    BOOST_CHECK(set1_1 == set1_2);
+    BOOST_CHECK(set1_2 == set1_1);
+    
+    BOOST_CHECK(set1_1 != set2_1);
+    BOOST_CHECK(set2_1 != set1_1);
+    
+    BOOST_CHECK(set1_1 != set3_1);
+    BOOST_CHECK(set3_1 != set1_1);
+    
+    BOOST_CHECK(set2_1 != set3_1);
+    BOOST_CHECK(set3_1 != set2_1);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
--- tests/utils.h
+++ tests/utils.h
@@ -38,17 +38,17 @@ template<unsigned int MOD>
 class mod_hash {
 public:   
     template<typename T>
-    size_t operator()(const T& value) const {
+    std::size_t operator()(const T& value) const {
         return std::hash<T>()(value) % MOD;
     }
 };
 
 class self_reference_member_test {
 public:
-    self_reference_member_test() : m_value(-1), m_value_ptr(&m_value) {
+    self_reference_member_test() : m_value(std::to_string(-1)), m_value_ptr(&m_value) {
     }
     
-    explicit self_reference_member_test(int64_t value) : m_value(value), m_value_ptr(&m_value) {
+    explicit self_reference_member_test(std::int64_t value) : m_value(std::to_string(value)), m_value_ptr(&m_value) {
     }
     
     self_reference_member_test(const self_reference_member_test& other) : m_value(*other.m_value_ptr), m_value_ptr(&m_value) {
@@ -71,7 +71,7 @@ public:
         return *this;
     }
     
-    int64_t value() const {
+    std::string value() const {
         return *m_value_ptr;
     }
 
@@ -93,15 +93,14 @@ public:
         return *lhs.m_value_ptr < *rhs.m_value_ptr;
     }
 private:    
-    int64_t  m_value;
-    int64_t* m_value_ptr;
+    std::string  m_value;
+    std::string* m_value_ptr;
 };
-
 
 
 class move_only_test {
 public:
-    explicit move_only_test(int64_t value) : m_value(new int64_t(value)) {
+    explicit move_only_test(std::int64_t value) : m_value(new std::string(std::to_string(value))) {
     }
     
     move_only_test(const move_only_test&) = delete;
@@ -148,26 +147,80 @@ public:
         }
     }
     
-    int64_t value() const {
+    std::string value() const {
         return *m_value;
     }
+    
 private:    
-    std::unique_ptr<int64_t> m_value;
+    std::unique_ptr<std::string> m_value;
 };
+
+
+class copy_only_test {
+public:
+    explicit copy_only_test(std::int64_t value): m_value(std::to_string(value)) {
+    }
+    
+    copy_only_test(const copy_only_test& other): m_value(other.m_value) {
+    }
+    
+    copy_only_test& operator=(const copy_only_test& other) {
+        m_value = other.m_value;
+        
+        return *this;
+    }
+    
+    ~copy_only_test() {
+    }
+    
+    
+    friend std::ostream& operator<<(std::ostream& stream, const copy_only_test& value) {
+        stream << value.m_value;
+        
+        return stream;
+    }
+    
+    friend bool operator==(const copy_only_test& lhs, const copy_only_test& rhs) { 
+        return lhs.m_value == rhs.m_value; 
+    }
+    
+    friend bool operator!=(const copy_only_test& lhs, const copy_only_test& rhs) { 
+        return !(lhs == rhs); 
+    }
+    
+    friend bool operator<(const copy_only_test& lhs, const copy_only_test& rhs) {
+        return lhs.m_value < rhs.m_value; 
+    }
+    
+    std::string value() const {
+        return m_value;
+    }
+    
+private:    
+    std::string m_value;
+};
+
 
 
 namespace std {
     template<>
     struct hash<self_reference_member_test> {
-        size_t operator()(const self_reference_member_test& val) const {
-            return std::hash<int64_t>()(val.value());
+        std::size_t operator()(const self_reference_member_test& val) const {
+            return std::hash<std::string>()(val.value());
         }
     };
     
     template<>
     struct hash<move_only_test> {
-        size_t operator()(const move_only_test& val) const {
-            return std::hash<int64_t>()(val.value());
+        std::size_t operator()(const move_only_test& val) const {
+            return std::hash<std::string>()(val.value());
+        }
+    };
+    
+    template<>
+    struct hash<copy_only_test> {
+        std::size_t operator()(const copy_only_test& val) const {
+            return std::hash<std::string>()(val.value());
         }
     };
 }
@@ -176,70 +229,80 @@ namespace std {
 class utils {
 public:
     template<typename T>
-    static T get_key(size_t counter);
+    static T get_key(std::size_t counter);
     
     template<typename T>
-    static T get_value(size_t counter);
+    static T get_value(std::size_t counter);
     
     template<typename HMap>
-    static HMap get_filled_hash_map(size_t nb_elements);
+    static HMap get_filled_hash_map(std::size_t nb_elements);
 };
 
 
 
 template<>
-inline int64_t utils::get_key<int64_t>(size_t counter) {
-    return boost::numeric_cast<int64_t>(counter);
+inline std::int64_t utils::get_key<std::int64_t>(std::size_t counter) {
+    return boost::numeric_cast<std::int64_t>(counter);
 }
 
 template<>
-inline self_reference_member_test utils::get_key<self_reference_member_test>(size_t counter) {
-    return self_reference_member_test(boost::numeric_cast<int64_t>(counter));
+inline self_reference_member_test utils::get_key<self_reference_member_test>(std::size_t counter) {
+    return self_reference_member_test(boost::numeric_cast<std::int64_t>(counter));
 }
 
 template<>
-inline std::string utils::get_key<std::string>(size_t counter) {
+inline std::string utils::get_key<std::string>(std::size_t counter) {
     return "Key " + std::to_string(counter);
 }
 
 template<>
-inline move_only_test utils::get_key<move_only_test>(size_t counter) {
-    return move_only_test(boost::numeric_cast<int64_t>(counter));
+inline move_only_test utils::get_key<move_only_test>(std::size_t counter) {
+    return move_only_test(boost::numeric_cast<std::int64_t>(counter));
+}
+
+template<>
+inline copy_only_test utils::get_key<copy_only_test>(std::size_t counter) {
+    return copy_only_test(boost::numeric_cast<std::int64_t>(counter));
 }
 
 
 
 
 template<>
-inline int64_t utils::get_value<int64_t>(size_t counter) {
-    return boost::numeric_cast<int64_t>(counter*2);
+inline std::int64_t utils::get_value<std::int64_t>(std::size_t counter) {
+    return boost::numeric_cast<std::int64_t>(counter*2);
 }
 
 template<>
-inline self_reference_member_test utils::get_value<self_reference_member_test>(size_t counter) {
-    return self_reference_member_test(boost::numeric_cast<int64_t>(counter*2));
+inline self_reference_member_test utils::get_value<self_reference_member_test>(std::size_t counter) {
+    return self_reference_member_test(boost::numeric_cast<std::int64_t>(counter*2));
 }
 
 template<>
-inline std::string utils::get_value<std::string>(size_t counter) {
+inline std::string utils::get_value<std::string>(std::size_t counter) {
     return "Value " + std::to_string(counter);
 }
 
 template<>
-inline move_only_test utils::get_value<move_only_test>(size_t counter) {
-    return move_only_test(boost::numeric_cast<int64_t>(counter*2));
+inline move_only_test utils::get_value<move_only_test>(std::size_t counter) {
+    return move_only_test(boost::numeric_cast<std::int64_t>(counter*2));
+}
+
+template<>
+inline copy_only_test utils::get_value<copy_only_test>(std::size_t counter) {
+    return copy_only_test(boost::numeric_cast<std::int64_t>(counter*2));
 }
 
 
 
 template<typename HMap>
-inline HMap utils::get_filled_hash_map(size_t nb_elements) {
+inline HMap utils::get_filled_hash_map(std::size_t nb_elements) {
     using key_t = typename HMap::key_type; using value_t = typename HMap:: mapped_type;
     
     HMap map;
     map.reserve(nb_elements);
     
-    for(size_t i = 0; i < nb_elements; i++) {
+    for(std::size_t i = 0; i < nb_elements; i++) {
         map.insert({utils::get_key<key_t>(i), utils::get_value<value_t>(i)});
     }
     
